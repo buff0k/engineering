a19e7e552b17e9d8083781ce063719b22f934578
Jorrie-Lab <jorrie@isambane.co.za>
2026-07-22 16:56:24 +0200

    Add Define Production Date controller

---
+++ engineering/engineering/doctype/define_production_date/define_production_date.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, Isambane Mining (Pty) Ltd
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class DefineProductionDate(Document):
+    pass
