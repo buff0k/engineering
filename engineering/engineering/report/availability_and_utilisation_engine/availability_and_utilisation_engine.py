829525a989904e1836a748cf37cef9295e76611c
Juan Swanepoel <juan@isambane.co.za>
2026-09-07 09:24:04 +0200

    Filter A&U assets by ownership

--- engineering/engineering/report/availability_and_utilisation_engine/availability_and_utilisation_engine.py
+++ engineering/engineering/report/availability_and_utilisation_engine/availability_and_utilisation_engine.py
@@ -250,6 +250,7 @@ def get_data(filters):
         planning_rows,
         selected_assets,
         companies,
+        filters.get("asset_ownership") or "Company Assets",
     )
 
     if not assets:
@@ -1573,6 +1574,7 @@ def get_assets(
     planning_rows,
     selected_assets,
     companies,
+    asset_ownership="Company Assets",
 ):
     locations = sorted({
         row.location
@@ -1617,6 +1619,15 @@ def get_assets(
 
         values["companies"] = tuple(
             companies
+        )
+
+    if asset_ownership == "Company Assets":
+        conditions.append(
+            "asset.asset_owner = 'Company'"
+        )
+    elif asset_ownership == "Supplier Assets":
+        conditions.append(
+            "asset.asset_owner = 'Supplier'"
         )
 
     assets = frappe.db.sql(
