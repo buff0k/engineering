4445ca10b407d87ff513b352e9fad57fd543b347
Juan Swanepoel <juan@isambane.co.za>
2026-09-08 06:04:46 +0200

    Name required-hours exceptions by site asset and day type

--- engineering/engineering/doctype/required_hours_exception_specification/required_hours_exception_specification.py
+++ engineering/engineering/doctype/required_hours_exception_specification/required_hours_exception_specification.py
@@ -3,6 +3,32 @@ from frappe.model.document import Document
 
 
 class RequiredHoursExceptionSpecification(Document):
+    def autoname(self):
+        first_exception = (
+            self.exceptions[0]
+            if self.exceptions
+            else None
+        )
+
+        asset = (
+            first_exception.asset
+            if first_exception
+            else "No-Asset"
+        )
+
+        day_type = (
+            first_exception.day_type
+            if first_exception
+            else "No-Day-Type"
+        )
+
+        unique_number = frappe.generate_hash(length=6).upper()
+
+        self.name = (
+            f"{self.location}-{asset}-{day_type}-"
+            f"{unique_number}"
+        )
+
     def validate(self):
         seen = set()
 
